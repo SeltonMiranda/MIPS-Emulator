d5ed6c709e27b30f41e1dcbec4f12fcae709e6b4
Selton <seltonmrolim@gmail.com>
2024-12-29 00:24:48 -0300

    adjustments

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ falta:
 Parei agora pois preciso fazer o trabalho de IC
 Quando estiver lendo isso novamente, a branch development é uma refatoraçao do codigo (está dando errado
 alguma coisa), continue até funcionar
+
+O plano é implementar todas as instruções e chamadas de sistema em [MIPS Instruction Set](https://www.dsi.unive.it/~gasparetto/materials/MIPS_Instruction_Set.pdf)
--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 
 #include "Engine.hpp"
 
-auto main() -> int {
-    std::string file = "example.txt";
+auto main(int argc, char *argv[]) -> int {
+  if (argc != 2) {
+    std::cout << std::format("Not enough arguments\n");
+    std::cout << std::format("Usage : {} <file.asm>\n", argv[0]);
+    return 0;
+  }
+
+  std::string text = std::string(argv[1]);
+  if (!text.contains(".asm")) {
+    std::cout << std::format("You must use only .asm files\n");
+    return 0;
+  }
+
   try {
-    auto tokenizer{new Emulator::Tokenizer{file}};
+    auto tokenizer{new Emulator::Tokenizer{text}};
     auto cpu{new Emulator::CPU()};
     auto emulator{new Emulator::Engine(tokenizer, cpu)};
     VecU8 code{emulator->assembler()};
