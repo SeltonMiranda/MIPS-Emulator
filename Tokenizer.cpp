c77a949b2173ba3f862bda272ecd42508a6b49a9
Selton <seltonmrolim@gmail.com>
2025-03-07 22:36:30 -0300

    Refactoring parseRegister function

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -29,12 +29,39 @@ mnemonicArgsSizeMap = {
   {"lw"  , 3},
 };
 
-static const std::unordered_map<std::string_view, u64>
-fixedRegisters = {
+static const std::unordered_map<std::string_view, u64> 
+RegisterNames = {
     {"zero",  0},
+    {"$at" ,  1},
     {"$v0" ,  2},
     {"$v1" ,  3},
+    {"$a0" ,  4},
+    {"$a1" ,  5},
+    {"$a2" ,  6},
+    {"$a3" ,  7},
+    {"$t0" ,  8},
+    {"$t1" ,  9},
+    {"$t2" , 10},
+    {"$t3" , 11},
+    {"$t4" , 12},
+    {"$t5" , 13},
+    {"$t6" , 14},
+    {"$t7" , 15},
+    {"$s0" , 16},
+    {"$s1" , 17},
+    {"$s2" , 18},
+    {"$s3" , 19},
+    {"$s4" , 20},
+    {"$s5" , 21},
+    {"$s6" , 22},
+    {"$s7" , 23},
+    {"$t8" , 24},
+    {"$t9" , 25},
+    {"$k0" , 26},
+    {"$k1" , 27},
+    {"$gp" , 28},
     {"$sp" , 29},
+    {"$fp" , 30},
     {"$ra" , 31},
 };
 
@@ -261,38 +288,19 @@ auto Tokenizer::validateArgumentsSize(const std::string& mnemonic, const VecStri
 }
 
 auto Tokenizer::parseRegister(const char *arg) -> u64 {
-
-  if (fixedRegisters.contains(std::string(arg))) { // Checks if it's a named register
-    return fixedRegisters.at(arg);
+  if (RegisterNames.contains(std::string(arg))) {
+    return RegisterNames.at(arg);
   }
 
   if (arg[0] != '$') {
     throw std::runtime_error{std::format("ERROR! Register doesn't exist {} \n", arg)};
   }
 
-  if (std::isdigit(arg[1])) {
-    return std::atoi(arg + 1);
+  if (!std::isdigit(arg[1])) {
+    throw std::runtime_error{std::format("ERROR! Register doesn't exist {} \n", arg)};
   }
 
-  char prefix{arg[1]};
-  u64 number;
-
-  if (1 < strlen(arg) && strlen(arg) <= 3) {
-    number = std::atoi(arg + 2);
-  } else {
-    throw std::invalid_argument(std::format("ERROR! No such a register {}\n", arg));
-  }
- 
-  switch (prefix) {
-  case 't':
-    return (number < 8) ? number + 8 : number + 16;
-  case 's':
-    return number + 16;
-  case 'a':
-    return number + 4;
-  default:
-    throw std::invalid_argument(std::format("ERROR! No such a register {}\n", arg));
-  }
+  return std::atoi(arg + 1);
 }
 
 auto Tokenizer::printTokens() -> void {
